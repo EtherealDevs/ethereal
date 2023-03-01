d61a85a5548f7cec8c5959461e2b505d46696dc8
joaccco <joaccofranc02@icloud.com>
2023-02-28 22:37:52 -0300

    Inicio de Proyecto

---
+++ src/components/background.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Background.css'; // archivo de estilos
+
+class Background extends React.Component {
+    render() {
+      const { color, children } = this.props;
+      const styles = {
+        backgroundColor: color,
+      };
+  
+      return (
+        <div className="background" style={styles}>
+          {children}
+        </div>
+      );
+    }
+  }
+
+  Background.propTypes = {
+    color: PropTypes.string.isRequired,
+  };
